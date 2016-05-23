504f1707dffb084d8a1fde543ae19695bea75f64
Evgeny Galperin <jjscout@gmail.com>
2016-05-23 16:48:23 +0300

    have function now

--- scripts/app.js
+++ scripts/app.js
@@ -2,24 +2,14 @@ var myApp = angular.module("myApp",[]);
 
 myApp.controller("mainController",function($scope){
 	//Controller code goes here
-	$scope.name="Boaty McBoatface";
-    $scope.shekel=1;
-    $scope.dollar=3.867;
     $scope.rs=0;
-    //expect(element(by.binding('rs')).getText()).toMatch('0');
-    //element(by.css('button')).click();
-    //expect(element(by.binding('rs')).getText()).toMatch('1');
-    $scope.toDollars=function(){
-        return $scope.shekel*$scope.dollar;
-    };
-    $scope.logRoughlySpeaking(){
+
+    $scope.addRoughlySpeaking=function(){
         $scope.rs = $scope.rs + 1;
-        
+        return;
+    }
+    $scope.removeRoughlySpeaking=function(){
+        $scope.rs = $scope.rs - 1;
+        return;
     }
 });
-
-//it('should check ng-click', function() {
-//  expect(element(by.binding('count')).getText()).toMatch('0');
-//  element(by.css('button')).click();
-//  expect(element(by.binding('count')).getText()).toMatch('1');
-//});
