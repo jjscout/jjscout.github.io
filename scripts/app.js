8b6e5a2bd6da52db2d1512bd02d0e698938e6005
Evgeny Galperin <jjscout@gmail.com>
2016-05-23 15:40:57 +0300

    starting to work on logging capability

--- scripts/app.js
+++ scripts/app.js
@@ -5,13 +5,17 @@ myApp.controller("mainController",function($scope){
 	$scope.name="Boaty McBoatface";
     $scope.shekel=1;
     $scope.dollar=3.867;
-    //$scope.rs;
+    $scope.rs=0;
     //expect(element(by.binding('rs')).getText()).toMatch('0');
     //element(by.css('button')).click();
     //expect(element(by.binding('rs')).getText()).toMatch('1');
     $scope.toDollars=function(){
         return $scope.shekel*$scope.dollar;
     };
+    $scope.logRoughlySpeaking(){
+        $scope.rs = $scope.rs + 1;
+        
+    }
 });
 
 //it('should check ng-click', function() {
