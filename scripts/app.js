78a9f3f0211372d58697d27aad52fc25fbd11b29
Evgeny Galperin <jjscout@gmail.com>
2016-05-30 13:50:43 +0300

    fixed less than zero count

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,17 @@ var myApp = angular.module("myApp",[]);
 
 myApp.controller("mainController",function($scope){
 	//Controller code goes here
-    $scope.rs=0;
-
+    $scope.rsCount=0;
+    $scope.log=[];
     $scope.addRoughlySpeaking=function(){
-        $scope.rs = $scope.rs + 1;
+        $scope.rsCount = $scope.rsCount + 1;
+        $scope.log[rsCount]={id:rsCount,timeTag:'1'};//new Date()}
         return;
     }
     $scope.removeRoughlySpeaking=function(){
-        $scope.rs = $scope.rs - 1;
+		if ($scope.rsCount > 0){
+			$scope.rsCount = $scope.rsCount - 1;
+		}
         return;
     }
 });
